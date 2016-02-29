6dd1389030d5e556c6e630b0f5a18a9249195ab5
Alumno <lynn@gmail>
2016-02-29 06:58:07 -0500

    practica 1

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>espinoza</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+><img src="imagen.1.jpg" alt="">
+<<img src="imagen .2.jpg" alt="">
+	<marquee>
+	<h1>espinoza doroteo edith</h1>
+</body>
+</html>
